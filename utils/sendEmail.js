a0f581f98b8bf7ef5c950b69a781fe5cfc9b474d
Raju Solanki <92461050+Rajusolanki01@users.noreply.github.com>
2024-02-24 21:34:55 +0530

    Changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { v4 as uuidv4 } from "uuid";
 import { hashString } from "./CreateJwt.js";
 import Verification from "../models/emailVerification.js";
-import PasswordReset from "../models/passwordReset.js"
+import PasswordReset from "../models/passwordReset.js";
 
 dotenv.config("./env");
 
@@ -28,12 +28,12 @@ export const sendVerificationEmail = async (user, res) => {
   const token = _id + uuidv4();
 
   //* Create a verification link
-  const link = APP_URL + "users/verify/" + _id + "/" + token;
+  const link = APP_URL + "/users/verify/" + _id + "/" + token;
 
   //* Mail options for the verification email
   const mailOptions = {
-    from: AUTH_EMAIL,
-    to: email,
+    from: email,
+    to: AUTH_EMAIL,
     subject: "Email Verification",
     html: `<div style='font-family: Arial, sans-serif; font-size: 20px; color: #333; background-color: #f7f7f7; padding: 20px; border-radius: 5px;'>
     <h3 style="color: rgb(8, 56, 188)">Please verify your email address</h3>
@@ -72,7 +72,8 @@ export const sendVerificationEmail = async (user, res) => {
         .then(() => {
           res.status(201).send({
             success: "PENDING",
-            message: "Verification email has been sent to your account. Check your email for further instructions.",
+            message:
+              "Verification email has been sent to your account. Check your email for further instructions.",
           });
         })
         .catch((err) => {
@@ -88,18 +89,18 @@ export const sendVerificationEmail = async (user, res) => {
 
 //* Function to send email for password reset
 export const resetPasswordLink = async (user, res) => {
-//* Destructure user object
+  //* Destructure user object
   const { _id, email } = user;
 
   //* Generate a unique token for password reset
   const token = _id + uuidv4();
   //* Create a password reset link
-  const link = APP_URL + "users/reset-password/" + _id + "/" + token;
+  const link = APP_URL + "/users/reset-password/" + _id + "/" + token;
 
   //* Mail options for the password reset email
   const mailOptions = {
-    from: AUTH_EMAIL,
-    to: email,
+    from: email,
+    to: AUTH_EMAIL,
     subject: "Password Reset",
     html: `<p style="font-family: Arial, sans-serif; font-size: 16px; color: #333; background-color: #f7f7f7; padding: 20px; border-radius: 5px;">
          Password reset link. Please click the link below to reset password.
